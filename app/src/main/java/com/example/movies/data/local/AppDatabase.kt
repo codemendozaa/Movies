f9c2df5fa9f7cc0062dece31a872deb71ea7f830
erixjavier <Mendhoza123#>
2021-11-22 18:11:26 -0500

    implementacion de Intance AppDabase

---
+++ app/src/main/java/com/example/movies/data/local/AppDatabase.kt
@@ -0,0 +1,31 @@
+package com.example.movies.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.movies.data.MovieEntity
+import org.w3c.dom.Entity
+
+
+@Database(entities =[MovieEntity::class],version = 1)
+abstract class AppDatabase :RoomDatabase(){
+    abstract fun movieDao():MovieDao
+
+    companion object{
+
+        private var INSTANCE:AppDatabase?=null
+
+        fun getDatabase(context: Context):AppDatabase{
+            INSTANCE = INSTANCE ?: Room.databaseBuilder(
+                context.applicationContext,
+                AppDatabase::class.java,
+                "movie_table"
+            ).build()
+            return INSTANCE!!
+        }
+        fun destroyInstance(){
+            INSTANCE = null
+        }
+    }
+}
